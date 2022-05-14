13cf472e4037905791085475757e562169bfec52
Duminike Vika <75427769+Sweet-Day@users.noreply.github.com>
2022-05-14 16:42:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
 import java.util.Arrays;
 
+/**
+ * 
+ * @author Vika
+ * 
+ */
+
 public class Main {
-
-    public static void main(String[] args) {
-        int [][] matrix = new int[3][3];
-        int x=0;
-
-        for(int[] row:matrix)
-            Arrays.fill(row,x);
-
-        for(int[] row:matrix)
-            System.out.println(Arrays.toString(row));
+    
+ /**
+  * creating and displaying a random matrix
+  * @param args 
+  */
+  public static void main(String[] args) {
+        int n = 3;
+        int[][] array = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i][j] = (int) (Math.random() * 10);
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("Column with max sum: " + (getColumnIndexMax(array)+1));
+        
+    }
+ 
+  /**
+   * Gets the column index max
+   * @param array
+   * @return index
+   */
+    private static int getColumnIndexMax(int[][] array) {
+        int sum = 0;
+        int index = 0;
+        for (int j = 0; j < array.length; j++) {
+            int currentColumn = 0;
+            for (int i = 0; i < array.length; i++) {
+                currentColumn += array[i][j];
+            }
+            if (sum == 0) {
+                sum = currentColumn;
+                index = j;
+            } else {
+                if (sum < currentColumn) {
+                    sum = currentColumn;
+                    index = j;
+                }
+            }
+ 
+        }
+        return index;
     }
 }
